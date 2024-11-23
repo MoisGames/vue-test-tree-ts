18970ff1be27e52f357fc4d371c3c726eff012ae
moisgames <VLAD981010@gmail.com>
2024-11-23 14:44:56 +0700

    chore(ModalWindow.vue) Создал новый компонент модального окна, и подключил его к MainPage.vue

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,17 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ModalWindow from './ModalWindow.vue';
+</script>
 
 <template>
   <main class="main-page__wrapper">
+    <ModalWindow />
     <v-button>Открыть</v-button>
   </main>
 </template>
 <style scoped>
 .main-page__wrapper {
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 100%;
   height: 90vh;
+  gap: 20px;
 }
 v-button {
   border: 2px solid black;
---
+++ src/components/ModalWindow.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="main-page__wrapper">
+    MODAL WINDOW
+  </main>
+</template>
+<style scoped>
+.main-page__wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 30%;
+  height: auto;
+  border: 2px solid black;
+  border-radius: 10px;
+  padding: 10px;
+}
+</style>
+
